82a38283a78c6d71b96c66699363f49bc7a772e6
Cikambai <firdaus.eeyangfu@gmail.com>
2026-03-10 03:04:58 +0800

    Add module imports

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -23,6 +25,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         },
       }),
     }),
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
